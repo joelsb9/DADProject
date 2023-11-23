4ac60c567f5b0f1d2ccf6df95ea815c26f99f5d1
Tiago Cabecinhas <tiago.pc2001@hotmail.com>
2023-11-23 20:04:15 +0000

    Feature[Login]: Barebones authentication implementation

--- vue/src/components/LaravelTester.vue
+++ vue/src/components/LaravelTester.vue
@@ -7,7 +7,7 @@ const username = ref('a1@mail.pt')
 const password = ref('123')
 const responseData = ref('')
 
-const submit = async () => {
+/*const submit = async () => {
     const responseLogin = await axios.post('/auth/login', {
         username: username.value,
         password: password.value
@@ -16,6 +16,34 @@ const submit = async () => {
     const responseRequest = await axios.get('/categories')
     responseData.value = responseRequest.data.data[0].name
 
+}*/
+
+const submit = async () => {
+    try {
+        // Correct the endpoint and provide the full URL
+        const responseLogin = await axios.post('http://127.0.0.1:8000/api/auth/login', {
+            username: username.value,
+            password: password.value
+        }, {
+            // Set the header to accept JSON
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+            }
+        });
+
+        // Set the authorization header
+        axios.defaults.headers.common.Authorization = "Bearer " + responseLogin.data.access_token;
+
+        // Correct the endpoint for the second request
+        const responseRequest = await axios.get('http://127.0.0.1:8000/api/categories');
+
+        // Update responseData with the appropriate data from the response
+        responseData.value = responseRequest.data.data[0].name;
+    } catch (error) {
+        // Handle errors (you might want to log or display an error message)
+        console.error('Error:', error);
+    }
 }
 </script>
 
